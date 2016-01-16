3457bda82e444f2a23b5990232d53a0ca32b6611
Ian Tokuyama <igolftokuyama@gmail.com>
2016-01-16 13:18:31 -0800

    Audio Assets
    Adding Audio assets and setting up a test sound scene

---
+++ Assets/Scripts/SoundTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTest : MonoBehaviour {
+
+    public AudioClip moveSound;
+
+    private AudioSource source;
+
+
+	// Use this for initialization
+	void Awake () {
+
+        source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        source.PlayOneShot(moveSound, 1.0f);
+
+	}
+}
