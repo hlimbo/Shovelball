66996eba0ae78d2149336decbd01d19a8abad1d1
hlimbo <limboh27@gmail.com>
2016-01-15 21:27:29 -0800

    Add PlayerMovement.cs and created Scripts folder

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    private Rigidbody2D body;
+    private float moveVelocity;
+
+    public float moveSpeed;
+    public float jumpSpeed;
+
+
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+        moveVelocity = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        moveVelocity = moveSpeed * Input.GetAxisRaw("Horizontal");
+
+        body.velocity = new Vector2(moveVelocity, body.velocity.y);
+
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+            
+        //}
+
+       
+
+	
+	}
+}
