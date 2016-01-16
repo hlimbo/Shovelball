2502e43d5b37155ebf7ef8e34ef3d49ddb5b37eb
Ted Won <tkwon09@gmail.com>
2016-01-16 01:56:33 -0800

    Refined movement controls on ground and air, added temporary sprites

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,14 +4,23 @@ using System.Collections.Generic;
 public class Movement : MonoBehaviour
 {
     public float maxMoveSpeed;
+    public float maxAirSpeed;
     public float moveAcceleration;
+    public float airAcceleration;
     public float maxFallSpeed;
     public float fallAcceleration;
     public float groundFriction;
+    public float airFriction;
+    public float groundPivotSpeedRetention;
+    public float airPivotSpeedRetention;
     public string playerInputIndex;
+
     public bool isGrounded;
 
     private Dictionary<string, Acceleration> accelerations;
+    private bool checkGroundedFlag = false;
+    private float previousDirection;
+    private float checkPlatformY;
 
 	// Use this for initialization
 	void Start ()
@@ -19,44 +28,103 @@ public class Movement : MonoBehaviour
         accelerations = new Dictionary<string, Acceleration>();
         accelerations.Add("Gravity", new Acceleration(null, maxFallSpeed, fallAcceleration));
         accelerations.Add("Movement", new Acceleration(null, null, moveAcceleration));
-        accelerations.Add("GroundFriction", new Acceleration(0.0f, null, groundFriction));
+        accelerations.Add("Friction", new Acceleration(null, null, 0.0f));
 	}
 	
-	// Update is called once per frame
+	// FixedUpdate is called once per physics step
 	void FixedUpdate ()
     {
-        //Input.GetAxisRaw("Horizontal" + playerInputIndex);
+        // Check if we are grounded.
+        if (checkGroundedFlag && IsAbovePlatform(checkPlatformY))
+        {
+            isGrounded = true;
+            checkGroundedFlag = false;
+            // disable gravity or else we create drag
+            accelerations["Gravity"].maxVelY = null;
+        }
 
-        //controllers
-        float direction = Input.GetAxis("Horizontal" + playerInputIndex) * maxMoveSpeed;
+        // Apply movement.
+        DoMove();
+
+        // Apply all accelerations.
+        ApplyAccelerations();
+	}
+
+    private void DoMove()
+    {
+        // Get the scaled movement direction depending on if grounded or in air.
+        float direction = Input.GetAxis("Horizontal" + playerInputIndex);
+        direction *= isGrounded ? maxMoveSpeed : maxAirSpeed;
+
+        float currAcceleration = isGrounded ? moveAcceleration : airAcceleration;
+        float currPivotSpeedRetention = isGrounded ? groundPivotSpeedRetention : airPivotSpeedRetention;
+        float currFriction = isGrounded ? groundFriction : airFriction;
+
+        // If the player is moving ...
         if (direction != 0)
         {
-            accelerations["GroundFriction"].magnitude = 0.0f;
+            // If the player's direction changed, set horizontal velocity to half. For responsiveness.
+            if (Mathf.Sign(previousDirection) != Mathf.Sign(direction))
+            {
+                SetVelocity(GetComponent<Rigidbody2D>().velocity.x * currPivotSpeedRetention, null);
+                GetComponent<Transform>().localScale = new Vector3(-Mathf.Sign(previousDirection), GetComponent<Transform>().localScale.y, GetComponent<Transform>().localScale.z);
+            }
+
+            // Disable friction and apply movement
+            accelerations["Friction"].maxVelX = null;
             accelerations["Movement"].maxVelX = direction;
+            accelerations["Movement"].magnitude = currAcceleration;
+
+            previousDirection = direction;
         }
+        // Player not moving ... apply friction and remove movement influence
         else
         {
-            accelerations["GroundFriction"].magnitude = groundFriction;
+            accelerations["Friction"].maxVelX = 0.0f;
+            accelerations["Friction"].magnitude = currFriction;
+            accelerations["Movement"].maxVelX = null;
         }
-
-
-            ApplyAccelerations();
-	}
+    }
 
     private void OnCollisionEnter2D (Collision2D collision)
     {
+        // If we hit the floor or platform ...
         if (collision.gameObject.tag == TagManager.floor || collision.gameObject.tag == TagManager.platform)
         {
-            isGrounded = true;
+            // Schedule a check for whether we landed on TOP of a platform. Need to do it in the next FixedUpdate since at the
+            // moment of collision, the colliders are intersecting and we can't accurately compare the colliders.
+            checkGroundedFlag = true;
+            checkPlatformY = collision.collider.transform.position.y + (((BoxCollider2D) collision.collider).size.y / 2);
         }
     }
 
     private void OnCollisionExit2D (Collision2D collision)
     {
+        // If we leave the floor or platform ...
         if (collision.gameObject.tag == TagManager.floor || collision.gameObject.tag == TagManager.platform)
         {
             isGrounded = false;
+            // re-enable gravity
+            accelerations["Gravity"].maxVelY = maxFallSpeed;
         }
+    }
+
+    private bool IsAbovePlatform (float platformy)
+    {
+        float playery = GetComponent<Transform>().position.y - (GetComponent<BoxCollider2D>().size.y / 2);
+        return platformy < playery;
+    }
+
+    // NULL values are PASS-THROUGH. AKA they do NOT change the player velocity.
+    private void SetVelocity(float? x, float? y)
+    {
+        Vector2 currentVelocity = GetComponent<Rigidbody2D>().velocity;
+        Vector2 newVelocity = new Vector2(currentVelocity.x, currentVelocity.y);
+        if (x.HasValue)
+            newVelocity.x = x.GetValueOrDefault();
+        if (y.HasValue)
+            newVelocity.y = y.GetValueOrDefault();
+        GetComponent<Rigidbody2D>().velocity = newVelocity;
     }
 
     private void ApplyAccelerations()
@@ -74,7 +142,7 @@ public class Movement : MonoBehaviour
                 targetVelocity.y = accel.maxVelY.GetValueOrDefault();
             
             // Linearly interpolate towards the new velocity
-            GetComponent<Rigidbody2D>().velocity = Vector2.MoveTowards(currentVelocity, targetVelocity, accel.magnitude);
+            GetComponent<Rigidbody2D>().velocity = Vector2.MoveTowards(currentVelocity, targetVelocity, accel.magnitude * Time.fixedDeltaTime);
         }
     }
 }
