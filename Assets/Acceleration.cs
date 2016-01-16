d3bc24ddaa4febc5467e6933f834d4b685250461
Ted Won <tkwon09@gmail.com>
2016-01-15 21:03:01 -0800

    Added acceleration class, filled in basics for the movement code, tweaked 2D physics settings

---
+++ Assets/Acceleration.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Acceleration
+{
+    public float? maxVelX;
+    public float? maxVelY;
+    public float magnitude;
+
+    public Acceleration(float? maxVelX, float? maxVelY, float magnitude)
+    {
+        this.maxVelX = maxVelX;
+        this.maxVelY = maxVelY;
+        this.magnitude = magnitude;
+    }
+}
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,15 +1,45 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
+    public float maxMoveSpeed = 8.0f;
+    public float moveAcceleration = 4.0f;
+    public float maxFallSpeed = -8.0f;
+    public float fallAcceleration = 2.0f;
+
+    private Dictionary<string, Acceleration> accelerations;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        accelerations = new Dictionary<string, Acceleration>();
+        accelerations.Add("Gravity", new Acceleration(null, maxFallSpeed, fallAcceleration));
+        accelerations.Add("Movement", new Acceleration(null, null, 0.0f));
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate ()
+    {
+	    
 	}
+
+    private void ApplyAccelerations()
+    {
+        foreach (Acceleration accel in accelerations.Values)
+        {
+            // Get current and target velocities
+            Vector2 currentVelocity = GetComponent<Rigidbody2D>().velocity;
+            Vector2 targetVelocity = new Vector2(currentVelocity.x, currentVelocity.y);
+
+            // If the target velocity has a non-null value, then use its value
+            if (accel.maxVelX.HasValue)
+                targetVelocity.x = accel.maxVelX.GetValueOrDefault();
+            if (accel.maxVelY.HasValue)
+                targetVelocity.y = accel.maxVelY.GetValueOrDefault();
+            
+            // Linearly interpolate towards the new velocity
+            GetComponent<Rigidbody2D>().velocity = Vector2.MoveTowards(currentVelocity, targetVelocity, accel.magnitude);
+        }
+    }
 }
