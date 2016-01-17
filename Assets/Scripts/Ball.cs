57f1882fb6ef82a2b0dc7592ccd350be0b3cc1c0
hlimbo <limboh27@gmail.com>
2016-01-16 16:22:24 -0800

    Updated Ball.cs script to work with PhysicsUtility

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -20,6 +20,7 @@ public class Ball : MonoBehaviour {
     public bool isOnWall;
 
     private Dictionary<string, Acceleration> accelerations;
+    private Rigidbody2D ballBody;
 
     // Use this for initialization
     void Start ()
@@ -28,11 +29,15 @@ public class Ball : MonoBehaviour {
         accelerations.Add("Gravity", new Acceleration(null,maxFallSpeed,fallAcceleration));
         accelerations.Add("Movement", new Acceleration(null,null, moveAcceleration));
         accelerations.Add("Jump", new Acceleration(null, null, 0.0f));
-	}
+
+        ballBody = GetComponent<Rigidbody2D>();
+
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
+        Vector2 oldVelocity = ballBody.velocity;
         bool wasGrounded = isGrounded;
         bool wasOnWall = isOnWall;
 
@@ -42,19 +47,22 @@ public class Ball : MonoBehaviour {
 
         if (isGrounded)
         {
-            accelerations["Gravity"].maxVelY = null;
-            SetVelocity(null, maxMoveSpeed);
+            //accelerations["Gravity"].maxVelY = null;
+            ballBody.velocity = PhysicsUtility.SetVelocity(ballBody.velocity,null, maxMoveSpeed);
+            
+            oldVelocity = ballBody.velocity;
         }
         else
         {
             //apply gravity here
-            accelerations["Gravity"].maxVelY = maxFallSpeed;
+            //accelerations["Gravity"].maxVelY = maxFallSpeed;
+            ballBody.velocity = PhysicsUtility.SetVelocity(ballBody.velocity, null, maxFallSpeed);
+         
+           // oldVelocity = ballBody.velocity;
+
         }
 
-
-
-       // SetVelocity(0, 30);
-        ApplyAccelerations();
+        ballBody.velocity = PhysicsUtility.ApplyAccelerations(oldVelocity, accelerations.Values);
 
 
 	}
@@ -68,35 +76,6 @@ public class Ball : MonoBehaviour {
 
     }
 
-    private void ApplyAccelerations()
-    {
-        foreach (Acceleration accel in accelerations.Values)
-        {
-            // Get current and target velocities
-            Vector2 currentVelocity = GetComponent<Rigidbody2D>().velocity;
-            Vector2 targetVelocity = new Vector2(currentVelocity.x, currentVelocity.y);
-
-            // If the target velocity has a non-null value, then use its value
-            if (accel.maxVelX.HasValue)
-                targetVelocity.x = accel.maxVelX.GetValueOrDefault();
-            if (accel.maxVelY.HasValue)
-                targetVelocity.y = accel.maxVelY.GetValueOrDefault();
-
-            // Linearly interpolate towards the new velocity
-            GetComponent<Rigidbody2D>().velocity = Vector2.MoveTowards(currentVelocity, targetVelocity, accel.magnitude * Time.fixedDeltaTime);
-        }
-    }
-
-    // NULL values are PASS-THROUGH. AKA they do NOT change the player velocity.
-    private void SetVelocity(float? x, float? y)
-    {
-        Vector2 currentVelocity = GetComponent<Rigidbody2D>().velocity;
-        Vector2 newVelocity = new Vector2(currentVelocity.x, currentVelocity.y);
-        if (x.HasValue)
-            newVelocity.x = x.GetValueOrDefault();
-        if (y.HasValue)
-            newVelocity.y = y.GetValueOrDefault();
-        GetComponent<Rigidbody2D>().velocity = newVelocity;
-    }
+   
 
 }
