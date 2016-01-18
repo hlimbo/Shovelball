39d891f8f6c837205b5d3a4083e94dc417acbf58
hlimbo <limboh27@gmail.com>
2016-01-17 19:18:39 -0800

    Add a bunch of random balls to use

---
+++ Assets/Scripts/RandomSpriteSelector.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSpriteSelector : MonoBehaviour {
+
+    public int numOfSprites;
+    public Sprite[] sprites;
+
+    private SpriteRenderer render;
+
+    void Awake()
+    {
+        render = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        Debug.Log("hello");
+        SetRandomSprite();
+    }
+
+    private void SetRandomSprite()
+    {
+        int spriteIndex = Random.Range(0, sprites.Length);
+        render.sprite = sprites[spriteIndex];
+        Debug.Log(spriteIndex);
+    }
+
+    void OnEnable()
+    {
+        SetRandomSprite();
+    }
+
+
+    
+}
