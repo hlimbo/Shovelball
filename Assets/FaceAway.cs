18cefaf4259a7485c6a6ead1536b3f2e2775a1d1
andresg6 <andresg6@uci.edu>
2016-01-17 22:12:37 -0800

    Added trail to ball

---
+++ Assets/FaceAway.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceAway : MonoBehaviour {
+
+    public int assignedPlayer;
+
+    private ParticleSystem ps;
+    private Ball b;
+	// Use this for initialization
+	void Start () {
+        ps = GetComponent<ParticleSystem>();
+        b = GetComponentInParent<Ball>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Always face opposite direction of parent's movement
+        Vector2 ballv = GetComponentInParent<Rigidbody2D>().velocity;
+        float angle = (Mathf.Atan2(ballv.y, ballv.x) / (2 * Mathf.PI)) * 360;
+        transform.rotation = Quaternion.AngleAxis(angle + 90, Vector3.forward);
+
+        //Enable and disable the emitter
+        if(ps.isStopped && b.isFlying)
+        {
+            ps.Play();
+        }
+        else if(ps.isPlaying && !b.isFlying)
+        {
+            ps.Stop();
+        }
+    }
+}
